65535dfe48495b7638246ed2f259056cf598403d
Devdeep Ghosh <63492939+thedevildude@users.noreply.github.com>
2021-09-06 07:45:04 +0530

    Tab added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const inputEl = document.getElementById("input-el")
 let inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
 
 const leadFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
@@ -11,6 +12,18 @@ if (leadFromLocalStorage) {
     myLeads = leadFromLocalStorage
     render(myLeads)
 }
+
+tabBtn.addEventListener("click", function() {
+
+    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+        
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+
+    })
+    
+})
 
 function render(leads) {
     let listItems = ""
